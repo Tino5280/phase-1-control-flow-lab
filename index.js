a276912b804610359da2523bd35d92e2befa718d
Tino <tino5280@gmail.com>
2023-02-22 19:50:13 -0700

    finished v2

--- index.js
+++ index.js
@@ -13,14 +13,22 @@ function scuberGreetingForFeet(rideLength) {
   }
   }
 
+  
+  
   function ternaryCheckCity(city) {
     return city === "NYC" ? "Ok, sounds good." : "No go."
   }
 
+  
+ 
+ 
   function switchOnCharmFromTip(tip) {
     switch (tip) {
       case 'generous' : return 'Thank you so much.';
       case 'not as generous' : return 'Thank you.'
       default : return 'Bye.'
     }
+ 
   }
+
+
